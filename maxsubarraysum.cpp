afb410be7e622015cad90a0bb67f347b63288610
NIkunj Miglani <niikkunjmiglani@gmail.com>
2025-12-18 16:08:59 +0530

    Create maxsubarraysum.cpp

---
+++ maxsubarraysum.cpp
@@ -0,0 +1,16 @@
+  int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int max_sum = INT_MIN;
+        int sum =0;
+        for(int i =0;i<n;i++){
+            sum = sum + nums[i];
+             if(sum > max_sum){
+                max_sum = sum;
+            }
+            if(sum<0){
+                sum =0;
+            }
+        }
+        return max_sum;
+    }
+};
